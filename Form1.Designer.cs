b1af6d806cd7c1e241b88bf00309fa0a10a0d46f
nitishvijai <nitish.vijai@hotmail.com>
2024-05-03 01:16:36 -0400

    Got list of all installed and open apps

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstBoxOpenApps = new System.Windows.Forms.ListBox();
+            this.lstBoxInstalledApps = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aboutBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "All currently open windows:";
+            // 
+            // lstBoxOpenApps
+            // 
+            this.lstBoxOpenApps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstBoxOpenApps.FormattingEnabled = true;
+            this.lstBoxOpenApps.Location = new System.Drawing.Point(15, 26);
+            this.lstBoxOpenApps.Name = "lstBoxOpenApps";
+            this.lstBoxOpenApps.Size = new System.Drawing.Size(401, 147);
+            this.lstBoxOpenApps.TabIndex = 2;
+            // 
+            // lstBoxInstalledApps
+            // 
+            this.lstBoxInstalledApps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstBoxInstalledApps.FormattingEnabled = true;
+            this.lstBoxInstalledApps.Location = new System.Drawing.Point(15, 198);
+            this.lstBoxInstalledApps.Name = "lstBoxInstalledApps";
+            this.lstBoxInstalledApps.Size = new System.Drawing.Size(401, 147);
+            this.lstBoxInstalledApps.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "All installed system apps:";
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.aboutBtn.Location = new System.Drawing.Point(341, 351);
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(75, 23);
+            this.aboutBtn.TabIndex = 5;
+            this.aboutBtn.Text = "About";
+            this.aboutBtn.UseVisualStyleBackColor = true;
+            this.aboutBtn.Click += new System.EventHandler(this.aboutBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(428, 382);
+            this.Controls.Add(this.aboutBtn);
+            this.Controls.Add(this.lstBoxInstalledApps);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxOpenApps);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sleep Mode v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstBoxOpenApps;
+        private System.Windows.Forms.ListBox lstBoxInstalledApps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button aboutBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,61 @@ namespace SleepMode
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> openApps = GetAllOpenApps();
+            List<string> installedApps = GetInstalledApps();
+            lstBoxOpenApps.DataSource = openApps;
+            lstBoxInstalledApps.DataSource = installedApps;
+        }
+
+        private List<string> GetAllOpenApps()
+        {
+            List<string> apps = new List<string>();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    apps.Add(process.MainWindowTitle + " <" + process.ProcessName + ">");
+                }
+            }
+            return apps;
+        }
+
+        private List<string> GetInstalledApps()
+        {
+            List<string> installedApps = new List<string>();
+            string registryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryPath))
+            {
+                foreach (string name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey app = key.OpenSubKey(name))
+                    {
+                        try
+                        {
+                            var displayName = app.GetValue("DisplayName");
+                            if (displayName != null)
+                            {
+                                installedApps.Add(displayName.ToString() + " <" + app.GetValue("InstallLocation") + ">");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                }
+            }
+            return installedApps;
+        }
+
+        private void aboutBtn_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutFrm = new AboutForm();
+            aboutFrm.ShowDialog();
         }
     }
 }
